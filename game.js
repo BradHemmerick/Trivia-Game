98547202431796e60af061a3edf46dd9f09cecaa
BradHemmerick <Brad.hemmerick13@gmail.com>
2017-10-17 13:14:15 -0500

    just style for right now

---
+++ game.js
@@ -0,0 +1,29 @@
+var TriviaContent = [
+    {
+        question: ["What is the longest running musical on broadway?"],
+        choices: ["The Phantom of the Opera", "Cats", "Chicago", "Disney's Aladdin"],
+        answer: 0
+    },
+    {
+        question: ["What is the top grossing broadwayshow of all time?"],
+        choices: ["The Book of Mormon", "Wicked", "Cats", "The Lion King"],
+        answer: 3
+    },
+    {
+        question: ["What was the name of the failed sequeal to The Phantom of the Opera?"],
+        choices: ["Christine & Phantom", "Return to the Opera", "The Phantom's Vengence", "Love Never Dies"],
+        answer: 3
+    },
+    {
+        question: ["Which Broadway musical has won the most awards?"],
+        choices: ["Beauty and the Beast", "South Pacific", "The Producers", "Hamiliton"],
+        answer: 1
+    },
+    {
+        question: ["What Musical set the record of $1.45 Billion in just one season?"],
+        choices: ["Cats", "Kinky Boots", "Hamilton", "Miss Saigon"],
+        answer: 2
+    },
+]
+
+var currentQuestion;
